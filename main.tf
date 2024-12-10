ef843e5a96a6232aca39156986d580cc4cfb711f
hammad-kamran <hammadkamran777@gmail.com>
2024-12-10 15:56:25 +0500

    Set up Terraform for GCP VM deployment

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  credentials = jsondecode(base64decode(var.gcp_credentials_file))
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11-bullseye-v20210916"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Assigns a public IP address
+    }
+  }
+}
