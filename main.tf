6a2bc09f436164cd011c4c041901e2d568ae502c
hammad-kamran <158271960+hammad-kamran@users.noreply.github.com>
2024-12-12 11:01:35 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = jsondecode(var.gcp_credentials_file)
+  credentials = file(var.gcp_credentials_file)
   project     = var.project_id
   region      = var.region
 }
